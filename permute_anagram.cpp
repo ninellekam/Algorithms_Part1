14b9751e636b589e599367aae90e425786c9549c
Нина Камкия <ninakamkia@MacBook-Air-NinelleKam.local>
2021-04-02 15:35:34 +0300

    Anagrams permute

--- permute_anagram.cpp
+++ permute_anagram.cpp
@@ -3,10 +3,9 @@
 #include <vector>
 #include <unordered_map>
 
-bool compare(std::unordered_map<int, int> my_txt, std::unordered_map<int, int> my_path) {
+bool compare(std::unordered_map<int, int> &my_txt, std::unordered_map<int, int> &my_path) {
 	for (int i = 0; i < 256; i++) {
 		if (my_path[i] != my_txt[i]) {
-			std::cout << i << " ";
 			return false;
 		}
 	}
@@ -33,14 +32,13 @@ bool	permute_anagram(const std::string &txt, const std::string &path) {
 	}
 	if (compare(my_txt, my_path))
 			return true;
-	std::cout << "no";
 	return false;
 }
 
 int main()
 {
-	const std::string txt = "BACDGABCDA";
-	const std::string path = "ABCD";
-	std::cout << permute_anagram(path, txt);
+	const std::string txt = "BADCDGABCDA";
+	const std::string path = "ABCDD";
+	std::cout << permute_anagram(txt, path);
 	return 0;
 }
